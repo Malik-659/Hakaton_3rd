ca2f39468fe97526e350efe77d77743878e7b860
kjbuhyvbhxhcbiusgrelgmkef <malik.vundiz@gmail.com>
2023-11-04 18:02:21 +0600

    L

--- src/components/maps/Map.jsx
+++ src/components/maps/Map.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Map = () => {
   return (
-    <div className="w-full m-auto">
+    <div className="flex justify-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="792.38214"
